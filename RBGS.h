a6cd98160583ef744aa62450b1fd5208623bd7ff
johnsmithm <moshnoi2000@gmail.com>
2015-11-28 22:07:04 +0100

    bad idea with no multiplication

--- RBGS.h
+++ RBGS.h
@@ -38,10 +38,10 @@ class RBGS{
 			
            for(size_t it=0;it<c__;++it){
 			   inx_ = inx_2 = 0;
-                 #pragma omp parallel for private(x,ptr,inx_,inx_2) schedule( static )
+                 #pragma omp parallel for private(x,ptr,inx_,inx_2)  schedule( static )
                 for(size_t i=1;i<ny_1;++i){//every black grid point  
-                    inx_ = inx_+nx_;
-                    inx_2 = inx_2+nx_2;
+                    inx_ = i*nx_;
+                    inx_2 = i*nx_2;
                     for(size_t j=((i&1)?1:2);j<nx__;j=j+2){
                         x = j/2+inx_2;
 						 ptr = (ur+x);
@@ -54,8 +54,8 @@ class RBGS{
 			   	inx_ = inx_2 = 0;
                  #pragma omp parallel for private(x,ptr,inx_,inx_2) schedule( static )
                 for(size_t i=1;i<ny_1;++i){//every red grid point
-                    inx_ = inx_+nx_;
-                    inx_2 = inx_2+nx_2;
+                    inx_ = i*nx_;
+                    inx_2 = i*nx_2;
                     for(size_t j=((i&1)?2:1);j<nx__;j=j+2){
                         x = j/2+inx_2;
 						ptr = (ub+x);
